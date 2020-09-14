33f6c2970dc5aba4e6154b334338164417deffe4
anketmanoj <anket.m@live.com>
2020-09-15 02:58:38 +0400

    Updates

---
+++ precache-manifest.6048729634621bfe7c7c3de6a30232d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b852ad6733a7538dcd06c06ed3424c28",
+    "url": "/ankets-eCommerce/index.html"
+  },
+  {
+    "revision": "70e4af7f59992ac8278a",
+    "url": "/ankets-eCommerce/static/css/main.60931552.chunk.css"
+  },
+  {
+    "revision": "a856f94dca0ed5a3132d",
+    "url": "/ankets-eCommerce/static/js/2.77a16f4b.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/ankets-eCommerce/static/js/2.77a16f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e4af7f59992ac8278a",
+    "url": "/ankets-eCommerce/static/js/main.64b7182f.chunk.js"
+  },
+  {
+    "revision": "479bcfea43955d911e47",
+    "url": "/ankets-eCommerce/static/js/runtime-main.ef47f23e.js"
+  }
+]);
